10f488c224151d9ab7e930bfb004e1199900cac4
Paprota <paprota404@gmail.com>
2024-08-21 16:01:40 +0200

    Delegates

--- Program.cs
+++ Program.cs
@@ -1,5 +1,13 @@
-﻿//New gear
+﻿using Delegates;
+
+//New gear
 //Stack and heap and JIT
 //Clang, GCC etc.
 //DDD and web servers
 //Transfer protocols and browsers
+
+public class Program{
+    static void Main(string[] args){
+        action("message");
+    }
+}
